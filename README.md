7e421eaf3480c92b4323607b9e2eb7c43bbccfa4
David Strootman <david@dutchmedialab.nl>
2017-09-07 14:12:51 +0200

    completed functionality and updated README.md

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # decimal - hexadecimal
-hexadecimal <> decimal conversion 
+decimal to hexadecimal conversion 
 
-### In development
-partially working with switch for single digit conversion.
-input / 16
+Call toHexadecimal(decimal,'datatype') on a decimal value to convert it to hexadecimal.
 
-if the result is larger than 16, get the decimals and multiply by 16.
-if the result is still larger than 16, repeat the division by 16 until the result is smaller than 16.
- convert all remainders and endresult to hexadecimals.
- Add these values from right to left in order to get the hexadecimal version of the input value.
+#####datatype accepts 'str' and 'arr'.  
+'str' returns the hexadecimal value as a string.
+'arr' returns the hexadecimal value as an array of strings.
--- converter.js
+++ converter.js
@@ -1,140 +1,84 @@
-/**
- * Created by David Strootman on 26-7-2017.
- */
 
-// function toRgb(hex) {
-// 	var result = toHexadecimal(parseInt(decimal / 16));
-// 	var remainder = toHexadecimal(decimal % 16);
-//
-// 	function toHexadecimal(decimal) {
-// 		var hexValue = '';
-// 		switch(decimal) {
-// 			case 0:
-// 				hexValue = '0';
-// 				break;
-// 			case 1:
-// 				hexValue = '1';
-// 				break;
-// 			case 2:
-// 				hexValue = '2';
-// 				break;
-// 			case 3:
-// 				hexValue = '3';
-// 				break;
-// 			case 4:
-// 				hexValue = '4';
-// 				break;
-// 			case 5:
-// 				hexValue = '5';
-// 				break;
-// 			case 6:
-// 				hexValue = '6';
-// 				break;
-// 			case 7:
-// 				hexValue = '7';
-// 				break;
-// 			case 8:
-// 				hexValue = '8';
-// 				break;
-// 			case 9:
-// 				hexValue = '9';
-// 				break;
-// 			case 10:
-// 				hexValue = 'A';
-// 				break;
-// 			case 11:
-// 				hexValue = 'B';
-// 				break;
-// 			case 12:
-// 				hexValue = 'C';
-// 				break;
-// 			case 13:
-// 				hexValue = 'D';
-// 				break;
-// 			case 14:
-// 				hexValue = 'E';
-// 				break;
-// 			case 15:
-// 				hexValue = 'F';
-// 				break;
-// 		}
-// 		return hexValue;
-// 	}
-// 	function convert(decimal) {
-// 		foreach
-// 	}
-// }
 
-Math.trunc = Math.trunc || function(x) {
-        return x - x%1;
-    };
-
-function toHexadecimal(input) {
+// function toHexadecimal
+function toHexadecimal(input, format) {
     var Hexadecimal = [];
-    var remainder = 0;
     var calculations = 0;
-    while (input >= 16) {
-        // if modulo return is smaller than 0, multiply by 16.
-        var decimal = input % 16;
-        Hexadecimal.push(convertValue(decimal));
-        input = Math.trunc(input / 16);
-        console.log("result: " + input +" " + "remainder: " + decimal);
-        calculations += 1;
-    }
-    console.log('amount of calculations:' + calculations);
-    function convertValue(decimal) {
-        var hexValue = '';
+    var decimal = 0;
+    var hexDigit = 0;
+    "use strict";
+    Math.trunc = Math.trunc || function (i) {
+            return i - i % 1;
+        };
+    function convertDigit(decimal) {
+        var hexValue = "";
         switch (decimal) {
-            case 0:
-                hexValue = '0';
-                break;
-            case 1:
-                hexValue = '1';
-                break;
-            case 2:
-                hexValue = '2';
-                break;
-            case 3:
-                hexValue = '3';
-                break;
-            case 4:
-                hexValue = '4';
-                break;
-            case 5:
-                hexValue = '5';
-                break;
-            case 6:
-                hexValue = '6';
-                break;
-            case 7:
-                hexValue = '7';
-                break;
-            case 8:
-                hexValue = '8';
-                break;
-            case 9:
-                hexValue = '9';
-                break;
-            case 10:
-                hexValue = 'A';
-                break;
-            case 11:
-                hexValue = 'B';
-                break;
-            case 12:
-                hexValue = 'C';
-                break;
-            case 13:
-                hexValue = 'D';
-                break;
-            case 14:
-                hexValue = 'E';
-                break;
-            case 15:
-                hexValue = 'F';
-                break;
+        case 0:
+            hexValue = "0";
+            break;
+        case 1:
+            hexValue = "1";
+            break;
+        case 2:
+            hexValue = "2";
+            break;
+        case 3:
+            hexValue = "3";
+            break;
+        case 4:
+            hexValue = "4";
+            break;
+        case 5:
+            hexValue = "5";
+            break;
+        case 6:
+            hexValue = "6";
+            break;
+        case 7:
+            hexValue = "7";
+            break;
+        case 8:
+            hexValue = "8";
+            break;
+        case 9:
+            hexValue = "9";
+            break;
+        case 10:
+            hexValue = "A";
+            break;
+        case 11:
+            hexValue = "B";
+            break;
+        case 12:
+            hexValue = "C";
+            break;
+        case 13:
+            hexValue = "D";
+            break;
+        case 14:
+            hexValue = "E";
+            break;
+        case 15:
+            hexValue = "F";
+            break;
         }
         return hexValue;
     }
-    console.log(Hexadecimal);
+    while (input >= 16) {
+        decimal = Math.trunc(input) % 16;
+        hexDigit = convertDigit(decimal);
+        Hexadecimal.push(hexDigit);
+        input = input / 16;
+        calculations += 1;
+    }
+
+    Hexadecimal.push(convertDigit(Math.trunc(input)));
+
+    Hexadecimal.reverse();
+    if (format === "str") {
+        return Hexadecimal.join("");
+    }
+    if (format === "arr") {
+        return Hexadecimal;
+    }
 }
